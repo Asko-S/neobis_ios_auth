589d678231c600c67a5fe68e2aecc366d900c87f
soronbekov-a-auca-2021 <soronbekov_a@auca.kg>
2023-09-11 15:15:11 +0600

    Model

---
+++ neobis_ios_auth/Models/UserModel.swift
@@ -0,0 +1,49 @@
+//  UserModel.swift
+//  neobis_ios_auth
+//  Created by Askar Soronbekov
+
+import Foundation
+
+struct ForgotPassword: Codable {
+    let email: String
+}
+
+struct ForgotPasswordConfirm: Codable {
+    let new_password: String
+    let new_password_confirm: String
+    let activation_code: String
+    
+    enum CodingKeys: String, CodingKey {
+        case new_password = "new_password"
+        case new_password_confirm = "new_password_confirm"
+        case activation_code = "activation_code"
+    }
+}
+
+struct PasswordChangeResponse: Decodable {
+    let msg: String
+}
+
+struct TokenObtainPair: Codable {
+    let refresh: String
+    let access: String
+}
+
+struct TokenRefresh: Codable {
+    let refresh: String
+    let access: String
+}
+
+struct RegisterBegin: Codable {
+    let email: String
+}
+
+struct Register: Codable {
+    let email: String
+    let name: String
+    let last_name: String
+    let birthday: String
+    let password: String
+    let password2: String
+}
+
